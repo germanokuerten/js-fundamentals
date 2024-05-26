3af3774000e07c81b2968771a898b06f8f91cd6a
Germano Kuerten <germanokuerten@gmail.com>
2024-05-25 23:47:19 -0300

    new commit

--- sections/control_flow/04_while_loop.js
+++ sections/control_flow/04_while_loop.js
@@ -36,21 +36,3 @@ while (i <= 5) {
 console.log('hello mundao');
 
 
-///////////////////
-
-// 5. Do... while loop
-
-// Do while loops are very similar to while loops, but with a slight difference. The difference is that in a Do while loop the code is executed as least once (even if the condition at the end evaluates to false).
-// in Do while loops, the variable also needs to be declared externally.
-
-// let i = 9;
-
-// do {
-//     if (i % 2 !== 0) {
-//         console.log('do while:', i) 
-//     }
-//     i++
-// } while (i <= 5)  
-
-
-// ps. Do while loops are not very used in the real world, most of the time you will be using a for loop or a while loop.
