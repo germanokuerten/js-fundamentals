138b0f41633775c3442f56ae223ed998648039de
Germano Kuerten <germanokuerten@gmail.com>
2024-05-25 23:53:38 -0300

    new commit

--- sections/control_flow/00_control_flow.js
+++ sections/control_flow/00_control_flow.js
@@ -5,41 +5,6 @@
 
 
 
-
-
-///////////////////
-
-// 6. Infinite Loops
-
-// It's a loop that executes infinitely. 
-// If you ended up creating one, it may crash your browser or your computer.
-
-//Examples. 
-
-// let i = 0
-// while (i < 5) {
-//     console.log(i)   
-// }
-
-// while (true){
-// }
-
-
-// do {
-// } while (true)
-
-// do {
-//     // x++ (forget to increment)
-// } while (x < 5)
-
-
-// for (let i = 1; i > 0; i++) {
-//     console.log('infinite:', i)
-// }
-
-// So avoid infinite loops.
-
-
 ///////////////////
 
 // 7. For...in loop
