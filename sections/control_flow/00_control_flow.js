a74b3f0f1430ded11d707b42ef69a27bd5381611
Germano Kuerten <germanokuerten@gmail.com>
2024-06-01 20:55:27 -0300

    new commit

--- sections/control_flow/00_control_flow.js
+++ sections/control_flow/00_control_flow.js
@@ -5,46 +5,7 @@
 
 
 
-///////////////////
 
-// 7. For...in loop
-
-// So far we've seen 3 kinds of loops.
-// for
-// while
-// do-while
-
-// But we have two more types of loops in JS, and we use them to iterate over the properties of an object, or elements in an array.
-
-const person1 = {
-    name: 'Mano',
-    age: 30,
-}
-
-// key is the loop variable (it can be any name)
-for (let key in person1) {
-    // to get the key
-    console.log('key:', key)
-    // to get the value (using bracket notation)
-    console.log('value:', person1[key])
-}
-
-// dot notation
-console.log(person1.name)
-
-// bracket notation  ['name of the target property']
-console.log(person1['name'])
-
-
-// so this is the for in loop and we use it to iterate over the properties of an object. 
-
-// ps. we can also use this to iterate over an array, but it's not ideal. But below is an example.
-
-const colors = ['red', 'green', 'blue']
-
-for (let index in colors) {
-    console.log(index, colors[index])
-}
 
 
 ///////////////////
