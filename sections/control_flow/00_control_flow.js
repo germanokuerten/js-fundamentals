a0b00e2f150df5af0ba2bde0d514f7dc23ec1ba3
Germano Kuerten <germanokuerten@gmail.com>
2024-06-08 23:36:55 -0300

    new commit

--- sections/control_flow/00_control_flow.js
+++ sections/control_flow/00_control_flow.js
@@ -5,27 +5,6 @@
 
 
 
-
-
-
-///////////////////
-
-// 8. For...of loop
-
-// for-of loop is the ideal way to iterate over arrays
-
-// because the loop variable (color) here holds one element/item at each iteration of the array. 
-for (let color of colors)
-    console.log(color)
-
-
-// Summary: 
-
-// So we use "for in" loop to iterate over properties of an object
-
-// and the "for of" loop to iterate over the elements/items in an array.
-
-
 ///////////////////
 
 // 9. Break and Continue 
