c4b8ffc542fadeae320c952ae88c00ef0948bf49
Germano Kuerten <germanokuerten@gmail.com>
2024-06-10 13:18:19 -0300

    new commit

---
+++ sections/control_flow/10_Exercise_max_of_2.js
@@ -0,0 +1,11 @@
+// Exercise: Greater of 2
+// Always test your functions with different possible values
+
+x = 21
+y = 29
+
+function maxOfTwo(x, y) {
+    return (x > y) ? x : y;
+}
+
+console.log(maxOfTwo(55, 55))
