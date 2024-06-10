bb685cc14c763bfa5d22b54fd915ab956908d04c
Germano Kuerten <germanokuerten@gmail.com>
2024-06-10 12:50:51 -0300

    new commit

--- sections/control_flow/08_for_of.js
+++ sections/control_flow/08_for_of.js
@@ -2,8 +2,14 @@
 // 8. For...of loop
 ///////////////////////
 
+let colors = ["green", "blue", "black"]
 
-// for-of loop is the ideal way to iterate over arrays
+for (let index in colors) {
+    console.log(index, colors[index])
+}
+// we use for in loop mostly to iterate of the properties of an object
+
+// for-of loop is the ideal way to iterate over in items/elements of an array
 
 // because the loop variable (color) here holds one element/item at each iteration of the array. 
 for (let color of colors)
