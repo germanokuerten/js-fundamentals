e33a153eebae5d158848fd341837f14a52727151
Germano Kuerten <germanokuerten@gmail.com>
2024-06-10 13:50:31 -0300

    new commit

---
+++ sections/control_flow/11_Exercise.js
@@ -0,0 +1,11 @@
+// function isLandscape(width, height) {
+//     return (width > height) ? true : false
+// }
+
+function isLandscape(width, height) {
+    return (width > height)
+}
+
+let landscape = isLandscape(233, 100)
+
+console.log(landscape)
---
+++ sections/control_flow/12_Exercise_fizzbuzz.js
@@ -0,0 +1,17 @@
+const output = fizzBuzz(false)
+console.log(output)
+
+
+function fizzBuzz(input) {
+    if (input / 1 !== input) return NaN
+    if (input % 3 === 0 && input % 5 === 0) return "FizzBuzz"
+    if (input % 3 === 0) return "fizz"
+    if (input % 5 === 0) return "Buzz"
+    return input
+} 
+
+// divisable by 3 = fizz
+// divisable by 5 = buzz
+// divisable by 3 and 5 = FizzBuzz
+// not divisable by 3 of 5 = same input
+// if dont pass a number get = not a number
